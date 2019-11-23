b73f8ab2fe0d2ff8483e5a912e26068590d34dc2
blaza168 <honzik.blazek@email.cz>
2019-11-23 01:42:15 +0100

    Code for trafic light

--- denis/app/application.c
+++ denis/app/application.c
@@ -3,12 +3,17 @@
 // LED instance
 bc_led_t led;
 
-int p = 0;
+int p = 0, q=0;
 
+//Funciton which is getting data from dongl
+void get_data(uint64_t *id, const char *topic, void *value, void *param);
+
+// LEDs
 bc_led_t red;
 bc_led_t yellow;
 bc_led_t green;
 
+//Taska
 bc_scheduler_task_id_t red_task;
 bc_scheduler_task_id_t yellow_task_on;
 bc_scheduler_task_id_t yellow_task_off;
@@ -16,6 +21,33 @@ bc_scheduler_task_id_t green_task;
 
 // Button instance
 bc_button_t button;
+
+static const bc_radio_sub_t data[] = {
+        {"alarm/-/set/state", BC_RADIO_SUB_PT_INT, get_data, NULL}
+};
+
+void get_data(uint64_t *id, const char *topic, void *value, void *param) {
+
+        bc_log_debug("data: " + *(int*)value);
+
+        if(*(int*)value == 0 && q == 0) //stoj
+        {
+
+            bc_scheduler_plan_from_now(red_task, 2500);
+            bc_led_set_mode(&yellow, BC_LED_MODE_TOGGLE);
+            bc_scheduler_plan_from_now(yellow_task_off, 2500);
+            bc_scheduler_plan_from_now(green_task, 2500);
+            q=1;
+        }
+        else if(*(int*)value == 1 && q == 1){ //jed
+
+            bc_led_set_mode(&green, BC_LED_MODE_TOGGLE);
+            bc_led_set_mode(&yellow, BC_LED_MODE_TOGGLE);
+            bc_scheduler_plan_from_now(yellow_task_off, 2500);
+            bc_scheduler_plan_from_now(red_task, 2500);
+            q=0;
+        } 
+}
 
 static void red_blink() {
     bc_led_set_mode(&red, BC_LED_MODE_TOGGLE);
@@ -42,19 +74,21 @@ void button_event_handler(bc_button_t *self, bc_button_event_t event, void *even
         if(p == 0) //stoj
         {
 
-            bc_led_set_mode(&green, BC_LED_MODE_TOGGLE);
+            bc_log_debug("stoj");
+            bc_scheduler_plan_from_now(red_task, 2500);
             bc_led_set_mode(&yellow, BC_LED_MODE_TOGGLE);
-            bc_scheduler_plan_from_now(yellow_task_off, 5000);
-            bc_scheduler_plan_from_now(red_task, 5000);
+            bc_scheduler_plan_from_now(yellow_task_off, 2500);
+            bc_scheduler_plan_from_now(green_task, 2500);
 
             p=1;
         }
         else if(p == 1){ //jed
 
-            bc_scheduler_plan_from_now(red_task, 5000);
+            bc_log_debug("jed");
+            bc_led_set_mode(&green, BC_LED_MODE_TOGGLE);
             bc_led_set_mode(&yellow, BC_LED_MODE_TOGGLE);
-            bc_scheduler_plan_from_now(yellow_task_off, 5000);
-            bc_scheduler_plan_from_now(green_task, 5000);
+            bc_scheduler_plan_from_now(yellow_task_off, 2500);
+            bc_scheduler_plan_from_now(red_task, 2500);
 
             p=0;
         }
@@ -71,7 +105,10 @@ void application_init(void)
 
     bc_radio_init(BC_RADIO_MODE_NODE_LISTENING);
 
-    bc_radio_pairing_request("semafor", VERSION);
+    bc_radio_pairing_request("semafor2", VERSION);
+
+    bc_radio_set_subs((bc_radio_sub_t *) data, sizeof(data) / sizeof(bc_radio_sub_t));
+
     // Initialize LED
     bc_led_init(&red, BC_GPIO_P0, false, false);
     bc_led_init(&yellow, BC_GPIO_P1, false, false);
@@ -83,7 +120,7 @@ void application_init(void)
 
     // Initialize traffic light to green
 
-    bc_led_set_mode(&green, BC_LED_MODE_ON);
+    bc_led_set_mode(&red, BC_LED_MODE_ON);
 
     // Initialize tasks
 
@@ -92,13 +129,4 @@ void application_init(void)
     yellow_task_off = bc_scheduler_register(yellow_off, NULL, BC_TICK_INFINITY);
     green_task = bc_scheduler_register(green_blink, NULL, BC_TICK_INFINITY);
 
-}
-
-void application_task(void)
-{
-    // Logging in action
-    bc_log_debug("application_task run");
-
-    // Plan next run this function after 1000 ms
-    bc_scheduler_plan_current_from_now(1000);
 }
